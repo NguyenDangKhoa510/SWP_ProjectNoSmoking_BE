8222ecfd89fe7ad2f6a386cc377d43ce8e068b9b
NguyenDangKhoa510 <dangkhoa5102004@gmail.com>
2025-06-12 12:18:08 +0700

    update_membershippackage

--- SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/MembershipPackageRepository.java
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/MembershipPackageRepository.java
@@ -2,7 +2,10 @@ package org.datcheems.swp_projectnosmoking.repository;
 
 import org.datcheems.swp_projectnosmoking.entity.MembershipPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-interface MembershipPackageRepository extends JpaRepository<MembershipPackage, Long> {
+
+@Repository
+public interface MembershipPackageRepository extends JpaRepository<MembershipPackage, Long> {
 
 }
