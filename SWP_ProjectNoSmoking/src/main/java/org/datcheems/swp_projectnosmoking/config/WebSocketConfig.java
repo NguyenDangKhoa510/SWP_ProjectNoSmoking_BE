e73c888a5f77315fca7150dbed0947a75b79d1c6
Kthichcode <ongquocdat2018@gmail.com>
2025-07-08 07:32:58 +0700

    Update WebSocketConfig.java

--- SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/config/WebSocketConfig.java
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/config/WebSocketConfig.java
@@ -34,7 +34,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
