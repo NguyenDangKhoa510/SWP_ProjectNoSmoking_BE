3af24c6e2cc5c8da946f8aa28b08d9b5170a92dd
MinKoThichCode <lequocminh6440@gmail.com>
2025-07-09 10:56:25 +0700

    Coach can see all member

---
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/dto/response/MemberProfileResponse.java
@@ -0,0 +1,21 @@
+package org.datcheems.swp_projectnosmoking.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+public class MemberProfileResponse {
+    private Long memberId;
+    private String username;
+    private String email;
+    private String fullName;
+    private String phoneNumber;
+    private LocalDate birthDate;
+    private String address;
+    private String gender;
+}
+
