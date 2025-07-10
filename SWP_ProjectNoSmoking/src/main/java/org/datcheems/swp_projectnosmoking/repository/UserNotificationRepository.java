9e34ebc91aad541c987d86349968c374aafc6be4
MinKoThichCode <lequocminh6440@gmail.com>
2025-07-10 11:13:43 +0700

    add Delete method Notification for ADMIN and COACH

--- SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/UserNotificationRepository.java
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/UserNotificationRepository.java
@@ -1,10 +1,21 @@
 package org.datcheems.swp_projectnosmoking.repository;
 
+import jakarta.transaction.Transactional;
 import org.datcheems.swp_projectnosmoking.entity.UserNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserNotificationRepository extends JpaRepository<UserNotification, Long> {
     List<UserNotification> findByUserId(Long userId);
-}
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM UserNotification u WHERE u.notification.id = :notificationId")
+    void deleteByNotificationId(Long notificationId);}
+
+
+
+
+
