b8337991d0564ccab5bbb88fc0d2b67ce25b68af
MinKoThichCode <lequocminh6440@gmail.com>
2025-07-10 14:30:11 +0700

    fix loi

--- SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/controller/NotificationController.java
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/controller/NotificationController.java
@@ -83,12 +83,7 @@ public class NotificationController {
     
 
 
-    @DeleteMapping("/{notificationId}")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('COACH')")
-    public ResponseEntity<String> deleteNotification(@PathVariable Long notificationId) {
-        notificationService.deleteNotification(notificationId);
-        return ResponseEntity.ok("Notification deleted successfully");
-    }
+  
 
 
 
