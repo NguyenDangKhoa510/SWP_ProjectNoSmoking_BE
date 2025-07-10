e2cf40919545bc81211cfbdd2fa27fd6019ba4b9
MinKoThichCode <lequocminh6440@gmail.com>
2025-07-10 11:13:43 +0700

    add Delete method Notification for ADMIN and COACH

--- SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/controller/NotificationController.java
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/controller/NotificationController.java
@@ -82,6 +82,15 @@ public class NotificationController {
 
 
 
+    @DeleteMapping("/{notificationId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('COACH')")
+    public ResponseEntity<String> deleteNotification(@PathVariable Long notificationId) {
+        notificationService.deleteNotification(notificationId);
+        return ResponseEntity.ok("Notification deleted successfully");
+    }
+
+
+
     @GetMapping("/history/{userId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('COACH')")
     public ResponseEntity<List<UserNotificationResponse>> getUserNotificationHistory(@PathVariable Long userId) {
