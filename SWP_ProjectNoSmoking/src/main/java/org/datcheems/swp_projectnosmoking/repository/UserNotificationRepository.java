c80d092f4dbc15d8b73d9886e30149f7efd5babb
MinKoThichCode <lequocminh6440@gmail.com>
2025-07-11 14:49:24 +0700

    add send for Coach, Member

--- SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/UserNotificationRepository.java
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/UserNotificationRepository.java
@@ -1,10 +1,12 @@
 package org.datcheems.swp_projectnosmoking.repository;
 
 import jakarta.transaction.Transactional;
+import org.datcheems.swp_projectnosmoking.entity.User;
 import org.datcheems.swp_projectnosmoking.entity.UserNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,6 +18,8 @@ public interface UserNotificationRepository extends JpaRepository<UserNotificati
     void deleteByNotificationId(Long notificationId);
 
     List<UserNotification> findByNotification_CreatedBy_Id(Long coachUserId);
+
+
 
 
 }
--- SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/UserRepository.java
+++ SWP_ProjectNoSmoking/src/main/java/org/datcheems/swp_projectnosmoking/repository/UserRepository.java
@@ -2,10 +2,13 @@ package org.datcheems.swp_projectnosmoking.repository;
 
 import org.datcheems.swp_projectnosmoking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,6 +20,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     long countByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.name IN :roleNames")
+    List<User> findByRoleNames(@Param("roleNames") List<String> roleNames);
+
 
 
 
